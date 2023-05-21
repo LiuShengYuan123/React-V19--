6957b00bdc96c3148e9b068c77aa96e6674230a7
ajian <azer6666@163.com>
2023-05-21 16:04:16 +0800

    feat: 第二课 添加注释

--- src/react/jsx.ts
+++ src/react/jsx.ts
@@ -27,29 +27,38 @@ const ReactElement = function (
 	return element;
 };
 
+// 1. 元素类型  2. 属性  3.子元素
 export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 	let key: Key = null;
 	const props: Props = {};
 	let ref: Ref = null;
 
+    // 遍历第二个参数对象中的属性
 	for (const prop in config) {
 		const val = config[prop];
+        
+        // 如果为key，赋值
 		if (prop === 'key') {
 			if (val !== undefined) {
 				key = '' + val;
 			}
 			continue;
 		}
+        // 如果为ref， 赋值
 		if (prop === 'ref') {
 			if (val !== undefined) {
 				ref = val;
 			}
 			continue;
 		}
+
+        // 给props赋值
 		if ({}.hasOwnProperty.call(config, prop)) {
 			props[prop] = val;
 		}
 	}
+
+    // 处理子元素
 	const maybeChildrenLength = maybeChildren.length;
 	if (maybeChildrenLength) {
 		if (maybeChildrenLength === 1) {
